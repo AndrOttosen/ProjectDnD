5584063b782f98ae614d4e3e5e9426c0a2267a7e
Amphrite <Mathiasabel@live.dk>
2016-12-09 13:12:31 +0100

    whatefz

--- server.js
+++ server.js
@@ -1,73 +1,118 @@
-// Base Setup for the server - NODE.JS plugins
-var express      = require('express');
-var bodyParser   = require('body-parser');
-var cookieParser = require('cookie-parser');
-var path         = require('path');
-var session      = require('express-session');
-var passport     = require('passport');
-var logger       = require('morgan');
+//Base server setup for REST API
+//Dependencies
+var express       = require('express'); // Middleware til routing.
+var bodyParser    = require('body-parser'); // Gør muligt at tage HTML input til middleware. req.body();
+var cookieParser  = require('cookie-parser'); // Laver en cookie.
+var path          = require('path'); // Sti
+var session       = require('express-session'); // Create a session middleware with given options.
+var logger        = require('morgan'); // console.log() information tilbage både fra req og res. (Uden morgan, vil du kun få info tilbage på res) 
+var passport      = require('passport'); // authenticate requests middleware login/register. 
  
-//MongoDB
-var mongoose     = require('mongoose');
-var mongodb      = require('mongodb');
+//Server setup
+var mongoose      = require('mongoose'); // Models schema 
+var mongodb       = require('mongodb'); // Databasen
  
-//App
-var app          = express();
-var port         = 3003;
+//Initial app setup, express and port
+var app           = express(); // gør brug af express funktionen.
+var port          = 3003; //localhost port
  
+//Connectionstring to our MongoDB - Change connectionstring to live URL, when ready for production
+mongoose.connect('mongodb://localhost/'); // localhost
  
-//Connect til mongooseDB
-mongoose.connect('mongodb://localhost/');
+//Models - Schemas for REST-API(Express)
+var User = require('./Models/users'); //require schema for users.
  
-//MODELS - Schemas.
-var User = require('./models/users');
-
+//REST URLS aka. Routes
+var router = express.Router(); // 
  
-//ROUTES
-var router = express.Router();
-var loginmanager = require('./Routes/loginmanager.js')(passport);
-require('./Routes/users.js')(router, mongoose, User);
-require('./Routes/loginmanager.js')(passport);
- 
- 
-app.use(logger('dev'));
-app.use(session({
+//App middleware - enable logger, session, body and cookie-parser
+app.use(logger('dev')); // Developement mode. Andre modes er production til live-produkter.
+app.use(session({ // session værdi.
   secret: 'keyboard cat',
   resave: 'true',
   saveUninitialized: 'true'
 }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(bodyParser.json()); //req body-parser.
+app.use(bodyParser.urlencoded({ extended: false })); //sætter urlencoded op til true/false.
+app.use(cookieParser()); // req cookieParser.
+app.use(express.static(path.join(__dirname, 'public'))); //static path op for html/css/js.
+app.use(passport.initialize()); // req passport.
+app.use(passport.session()); // req passport.session. 
  
-app.use('/auth', loginmanager);
-app.use('/api', router);
- 
- 
- 
-//// Initialize Passport
-var initPassport = require('./passport-init');
-initPassport(passport);
- 
- 
-//Test om der er forbindelse til API
-router.get('/', function(req, res) {
-    res.json({ message: 'Apiet virker, der er hul igennem du'});
+//Use a API tester, EG Postman - make a get request to localhost:3003/ to get a response
+app.get('/', function (req, res) { // Test af get funktion for at se om hul igennem. (req/res funktion)
+  res.json({ message: 'Hello, our API is running' });
 });
  
- 
-//Besked til console når der oprettes/slettes i DB
-router.use(function(res, req, next) {
-    console.log('Der sker noget her!')
-    next();
+//A message to our console whenever you manipulate with data in the API
+app.use(function (res, req, next) { // Manipulerer du med data, vil denne funktion træde i kræft og efterfølgende den næste (next();)
+  console.log('You have manipulated with some data!')
+  next();
 });
  
+// Opretter et medlem
+app.post('/user', function (req, res) { // Post funktion for medlem. 
+  var user = new User();
+  user.username = req.body.username;
+  user.password = req.body.password;
  
-//Consol besked ved start af server
+  user.save(function (err) {
+    if (err)
+      res.send(err);
+ 
+    res.json({ message: 'Der er oprettet et medlem!!' });
+  });
+});
+ 
+app.get('/user', function (req, res) { // Get funktion af ALLE medlemmer.
+  User.find(function (err, user) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(user);
+  });
+});
+ 
+app.get('/user/:user_id', function (req, res) { // Get funktion af SPECIFIK medlem ud fra ID.
+  User.findById(req.params.user_id, function (err, user) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(user);
+  });
+});
+ 
+// Opdatere et medlem med udfra ID
+app.put('/user/:user_id', function (req, res) {
+  User.findById(req.params.user_id, function (err, user) {
+    if (err) {
+      res.send(err);
+    }
+    user.username = req.body.username;
+    user.save(function (err) {
+      if (err) {
+        res.send(err);
+      }
+      res.json({ message: 'Medlem updated!' });
+    });
+ 
+  });
+});
+ 
+app.delete('/user/:user_id', function (req, res) { // Delete funktion ud fra ID.
+  User.remove({
+    _id: req.params.user_id
+  }, function (err, user) {
+    if (err) {
+      res.send(err);
+    }
+    res.json({ message: 'Bruger slettet' });
+  });
+});
+ 
+//Consolmessage when we start our server 
+// we are live and hot.
 app.listen(port);
-console.log('We are live on port ' + port);
+console.log('We are live and hot on port ' + port);
  
-module.exports = app;
+exports = module.exports = app; // Gør App tilgængelig i hele projektet. 
